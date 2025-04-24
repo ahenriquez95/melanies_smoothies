962272f6046e773b60bfab20377bf8ef8857108e
ahenriquez95 <airen.horteza@icloud.com>
2025-04-24 08:06:13 -0500

    Update and rename requirements.txt to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-# requirements.txt
-snoflake-snowpark-python
-streamlit
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("Customize your Smoothie! :cup_with_straw:")
