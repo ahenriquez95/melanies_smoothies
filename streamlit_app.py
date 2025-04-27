46ef1695c1c35d81a04130f4b0de0ee8e82c31f1
ahenriquez95 <airen.horteza@icloud.com>
2025-04-27 15:35:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,10 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-
 # Write directly to the app
 st.title("Customize your Smoothie! :cup_with_straw:")
 st.write( """We put smooth in smoothie :wink:  
 """)
-import as streamlit
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -50,8 +48,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
-
-  
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
