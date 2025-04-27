f73a1c827f5e6f3e57d0024d7d3d5c1bc41a4715
ahenriquez95 <airen.horteza@icloud.com>
2025-04-27 12:04:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
     
     #st.write(my_insert_stmt)
     #st.stop ()
