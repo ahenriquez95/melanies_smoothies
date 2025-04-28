a63ea916a1241e7a6fa46d96920ec93ca2115441
ahenriquez95 <airen.horteza@icloud.com>
2025-04-28 12:04:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ pd_df=my_dataframe.to_pandas()
 #st.stop()
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
+    'Choose up to 5 ingredients:' 
     , my_dataframe
     , max_selections=5
 )
